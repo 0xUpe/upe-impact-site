f235c5df0209166dc9bab7854bcbb5bb567c73d0
4d4n <git@keenintel.com>
2024-06-20 12:00:29 -0600

    WhatIsUpe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,13 +39,14 @@ export default function Home() {
 					<>
 						<div className="h-1 bg-gradient-to-r from-pink-600 via-red-500 to-yellow-400 rounded mb-10 w-11/12 mx-auto"></div>
 						
-					<Badges />
-					<OCS />
-					<div className="allies-outer bg-upe-gradient w-full p-2 md:p-6 lg:p-8 max-w-screen-xl">
-						<Allies />
-						<Projects />
-					</div>
-					<Join />	
+						<Badges />
+						<WhatIsUpe />
+						<OCS />
+						<div className="allies-outer bg-upe-gradient w-full p-2 md:p-6 lg:p-8 max-w-screen-xl">
+							<Allies />
+							<Projects />
+						</div>
+						<Join />	
 					</>
 				)}
 
@@ -187,7 +188,7 @@ function Badges() {
   return (
 	      <div className="badges-outer bg-upe-gradient w-full p-2 md:p-6 lg:p-6 max-w-screen-xl">
       		  <div className="badges-inner bg-upe-grey px-4 py-8 sm:px-6 lg:px-8 mx-auto max-w-screen-lg rounded-xl">
-        		<h2 className="text-xl text-center mb-10">Upe Impact Badges for Local Communities in Guanacaste, Costa Rica</h2>
+        		<h2 className="text-xl text-center mb-10"><span className="text-upe-pink">Upe Impact</span> Badges for Local Communities in Guanacaste, Costa Rica</h2>
         		<div className="grid gap-4 grid-cols-2 md:grid-cols-2 lg:grid-cols-4 justify-center mx-auto w-[fit-content]">
 					<BadgeItem
 					title="Upe Local Community Ally"
@@ -247,7 +248,7 @@ function OCS() {
 		  <div className="h-1 bg-gradient-to-r from-pink-600 via-red-500 to-yellow-400 rounded my-20 w-11/12 mx-auto"></div>
 		  <div className="flex items-center justify-center mt-20">
     		<Image src={upeIcon} alt="" width={60} height={60} style={{ filter: "drop-shadow(0px 0px 24px #a726a9a8)" }} />
-			  <h3 className="text-xl text-center ml-4">Upe Impact was built during</h3>			  
+			  <h3 className="text-xl text-center ml-4"><span className="text-upe-pink">Upe Impact</span> was built during</h3>			  
 		  </div>
 		  	<Image src="/home/onchainsummerw.png" width={946} height={279} alt="Onchain Summer" className="mx-auto" />
 			<div className="grid gap-4 grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-center items-start mx-auto w-[fit-content]">
@@ -353,6 +354,26 @@ function Projects() {
         />
       </div>
     </div>
+  );
+}
+
+function WhatIsUpe() {
+	return (
+	<div className="what-is-upe-outer bg-white w-full p-2 md:p-6 lg:p-6 max-w-screen-xl">
+		<div className="what-is-upe-inner w-full p-2 md:p-6 lg:p-6 max-w-screen-lg flex flex-col md:flex-row items-center justify-center mx-auto bg-white rounded border-upe-grey border-4 rounded-xl grid grid-cols-1 md:grid-cols-2 gap-10">
+			<div className="flex flex-col items-center">
+					<h2 className="text-4xl font-bold mb-4 text-upe-grey">WHAT IS <span className="text-upe-pink">UPE IMPACT</span>?</h2>
+					<div className="w-full">
+						<Image src="/home/arrow.png" alt="Upe Impact" width={484} height={241} />
+					</div>
+			</div>
+			<div className="flex flex-col items-center">
+				<p className="mb-4 text-upe-grey"><span className="text-upe-pink">Upe Impact</span> is a web app designed to facilitate the coordination of community betterment projects and track their results on-chain. With an integrated authentication system the application generates user smart accounts upon login, ensuring secure access for all users, regardless of their technical expertise, from web3 natives to novices.
+				</p>
+				<a href="#" rel="noopener noreferrer" className="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded bg-upe-pink">LEARN MORE</a>
+			</div>
+		</div>
+	</div>
   );
 }
 
