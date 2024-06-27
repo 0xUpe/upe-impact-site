e2b56ef085f91e1ef747a22f49b92385f5946532
4d4n <git@keenintel.com>
2024-06-27 13:00:10 -0600

    main-net

--- src/app/trophy-case/page.tsx
+++ src/app/trophy-case/page.tsx
@@ -49,7 +49,7 @@ const BadgesHome: React.FC = () => {
 	}, [ownedNftsData]);
 
 	const openSeaUrl = (contractAddress: string, tokenId: bigint) => {
-		return `https://testnets.opensea.io/assets/base-sepolia/${contractAddress}/${tokenId}`;
+		return `https://opensea.io/assets/base-sepolia/${contractAddress}/${tokenId}`;
 	};
 
 	return (
